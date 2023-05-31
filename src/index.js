0be2cb8e1b60052808f4be34cd74ceac0ceff12f
cany <couleurvide1008@gmail.com>
2023-05-31 12:14:31 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const Hello = (props) => {
+  return (
+    <div>こんにちは、{props.children}さん</div>
+  );
+};
+
+const Greeting = (props) => {
+  return (
+    <div>
+      <div>ご挨拶</div>
+      {props.children}
+    </div>
+  );
+};
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Greeting>
+      <Hello>西郷隆盛</Hello>
+      <Hello>坂本龍馬</Hello>
+    </Greeting>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
