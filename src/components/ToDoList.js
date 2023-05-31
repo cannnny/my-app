9cdd27a9c9a450d221e38aeb2f3453a4366ac93a
cany <couleurvide1008@gmail.com>
2023-05-31 17:14:48 +0900

    タスク入力欄の追加

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -19,10 +19,20 @@ const TodoList = () => {
 
     // useStateにinitialStateを指定すると、todosにTodoリストが保存される
     const [todos, setTodos] = useState(initialState);
+    // 入力したタスクの情報を保持するための変数を追加
+    const [task, setTask] = useState('');
+    // イベント時に実行される関数を定義
+    const handleNewTask = (event) => {
+        setTask(event.target.value);
+    }
 
     return(
         <div>
             <h1>TODO LIST</h1>
+            Add task : <input 
+                value={task}
+                placeholder='Add new Task'
+                onChange={handleNewTask} />
             <ul>
                 {todos.map((todo, index) => (
                     <li key={index}>{todo.task}</li>
