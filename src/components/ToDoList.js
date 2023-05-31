5df4689acd00b8d93423ebcb4411ec2fed74125d
cany <couleurvide1008@gmail.com>
2023-05-31 19:43:33 +0900

    タスク更新機能の追加

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -41,11 +41,22 @@ const TodoList = () => {
     }
 
     // 削除時に実行される関数を定義
-    const handleRemoveTask = (index) => {
-        const currentTodos = [...todos]
-        currentTodos.splice(index, 1)
-        setTodos(currentTodos)
-    }
+    // const handleRemoveTask = (index) => {
+    //    const currentTodos = [...todos]
+    //    currentTodos.splice(index, 1)
+    //    setTodos(currentTodos)
+    //}
+
+    // 削除時にタスクを完了させる関数を定義
+    const handleUpdateTask = (index) => {
+        let currentTodos = todos.map((todo, todoIndex) => {
+            if(todoIndex === index){
+                todo.isCompleted = !todo.isCompleted
+            }
+            return todo;
+        })
+        setTodos(currentTodos);
+    };
 
     return(
         <div>
@@ -60,8 +71,9 @@ const TodoList = () => {
             </form>
             <ul>
                 {todos.map((todo, index) => (
-                    <li key={index}>{todo.task}<span onClick={() => handleRemoveTask(index)}>
-                     ✗</span></li>
+                    <li key={index}
+                        style={todo.isCompleted === true ? {textDecorationLine: 'line-through'} : {} }>
+                        {todo.task}<span onClick={() => handleUpdateTask(index)}>✗</span></li>
                 ))}
             </ul>
         </div>
