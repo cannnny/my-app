d5d8336264851aa14a53c43d111d1a0a53337300
cany <couleurvide1008@gmail.com>
2023-06-01 11:25:40 +0900

    チェックボックスの追加

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -40,12 +40,12 @@ const TodoList = () => {
         setNewTodo('')
     }
 
-    // 削除時に実行される関数を定義
-    // const handleRemoveTask = (index) => {
-    //    const currentTodos = [...todos]
-    //    currentTodos.splice(index, 1)
-    //    setTodos(currentTodos)
-    //}
+    //削除時に実行される関数を定義
+    const handleRemoveTask = (index) => {
+       const currentTodos = [...todos]
+       currentTodos.splice(index, 1)
+       setTodos(currentTodos)
+    }
 
     // 削除時にタスクを完了させる関数を定義
     const handleUpdateTask = (clickIndex) => {
@@ -72,8 +72,21 @@ const TodoList = () => {
             <ul>
                 {todos.map((todo, index) => (
                     <li key={index}
-                        style={todo.isCompleted === true ? {textDecorationLine: 'line-through'} : {} }>
-                        {todo.task}<span onClick={() => handleUpdateTask(index)}>✗</span></li>
+                        style={{textDecoration: todo.isCompleted ? 'line-through': 'none'}}
+                    >
+                        <input
+                            type="checkbox"
+                            checked={todo.isCompleted}
+                            onChange={() => handleUpdateTask(index)}
+                        />
+                        {todo.task}
+                        <span
+                            onClick={() => handleRemoveTask(index)}
+                            style={{cursor: 'pointer'}}
+                        >
+                            ✗
+                        </span>
+                    </li>
                 ))}
             </ul>
         </div>
